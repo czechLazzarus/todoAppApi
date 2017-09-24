298a2ce1abd4977b19b1df7098cb58e68c40a1b2
czechLazzarus <masterbolt@centrum.cz>
2017-09-24 12:18:34 +0200

    Update GetTodoCept.php

--- tests/api/GetTodoCept.php
+++ tests/api/GetTodoCept.php
@@ -4,6 +4,5 @@ $I = new ApiTester($scenario);
 $I->wantTo('Testing get specific todo');
 $I->sendGET('todos/1');
 
-
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 $I->seeResponseIsJson();
