aec0bf4874b3d2475f490f40b7be48ef118c48ba
czechLazzarus <masterbolt@centrum.cz>
2017-09-24 12:18:17 +0200

    Update GetTodosCept.php

--- tests/api/GetTodosCept.php
+++ tests/api/GetTodosCept.php
@@ -6,6 +6,5 @@ $I->wantTo('Testing get all todos');
 $I->haveHttpHeader('Content-Type', 'application/json');
 $I->sendGET('todos');
 
-
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 $I->seeResponseIsJson();
